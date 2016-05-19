fce932dcc1963c470923ed136b882f7d75089a26
johnnypesola <johnny@pesola.eu>
2016-05-19 18:13:47 +0200

    Blog post update

--- react-router-redux/components/BlogPost.js
+++ react-router-redux/components/BlogPost.js
@@ -2,6 +2,29 @@ import React from 'react'
 
 export default function BlogPost() {
   return (
-    <div>Should be a blog post</div>
+    <h2>
+      {{post.header}}
+    </h2>
+    <p>
+      {{post.content}}
+    </p>
+    <h3>
+      Date
+    </h3>
+    {{post.timestamp | date:'yyyy-MM-dd HH:mm:ss Z'}}
+    <h3>
+      Author
+    </h3>
+    <img width="50" ng-src="app/img/{{post.picture}}" alt="{{post.name}}"> {{post.name}}<br>
+    Email: {{post.email}}<br>
+
+    <h3>Tags</h3>
+    <ul>
+      <li ng-repeat="tag in post.tags">
+        {{tag}}
+      </li>
+    </ul>
+
+    <a href="#/">Back</a>
   )
 }
